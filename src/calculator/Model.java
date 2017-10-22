15604bcb151e7a1d3947c3216bf8121c1da9b91c
martip23 <patrickmartinez77@gmail.com>
2017-10-21 21:50:45 -0500

    First commit

---
+++ src/calculator/Model.java
@@ -0,0 +1,10 @@
+package calculator;
+
+/**
+ * @author martip23
+ * This interface must be implemented by all classes that wish to play the Model
+ * role within the MVC framework.
+ */
+public interface Model {
+	void notifyChanged(ModelEvent event);
+}
